cf6881916d32249f2d440a2b63feee34fba699e6
gustavo espindola <espindolage@gmail.com>
2012-11-25 15:45:13 -0300

    ..

--- files/assets/js/plugins.js
+++ files/assets/js/plugins.js
@@ -4,4 +4,24 @@
  	$('.goHome').click(function(e){
 		location.href= "/dashboard";
 	});
+
+
+ /*
+ 	//iscroll
+
+	var myScroll;
+	function loaded() {
+		setTimeout(function () {
+			myScroll = new iScroll('wrapper');
+		}, 100);
+	}
+	window.addEventListener('load', loaded, false);
+
+	var myScroll = new iScroll('wrapper', { hScrollbar: false, vScrollbar: false });
+*/
+
+	//NiceScroll
+ 	$("body").niceScroll();
+ 	$(".scroll").niceScroll();	
+ 
 });
